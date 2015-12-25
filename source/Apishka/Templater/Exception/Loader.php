4de6259d590799337f4e9078cfed8dd6c2b0abdd
Alex Lobtsov <dessander@gmail.com>
2015-12-25 18:15:03 +0400

    Update loader exception

--- source/Apishka/Templater/Exception/Loader.php
+++ source/Apishka/Templater/Exception/Loader.php
@@ -1,31 +1,12 @@
 <?php
 
-/*
- * This file is part of Twig.
+/**
+ * Apishka templater exception loader
  *
- * (c) 2010 Fabien Potencier
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * @uses Apishka_Templater_Exception
+ * @author Alexander "grevus" Lobtsov <alex@lobtsov.com>
  */
 
-/**
- * Exception thrown when an error occurs during template loading.
- *
- * Automatic template information guessing is always turned off as
- * if a template cannot be loaded, there is nothing to guess.
- * However, when a template is loaded from another one, then, we need
- * to find the current context and this is automatically done by
- * Twig_Template::displayWithErrorHandling().
- *
- * This strategy makes Twig_Environment::resolveTemplate() much faster.
- *
- * @author Fabien Potencier <fabien@symfony.com>
- */
-class Twig_Error_Loader extends Twig_Error
+class Apishka_Templater_Exception_Loader extends Apishka_Templater_Exception
 {
-    public function __construct($message, $lineno = -1, $filename = null, Exception $previous = null)
-    {
-        parent::__construct($message, false, false, $previous);
-    }
 }
