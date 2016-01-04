44453c28b3a67ac0311b1c8b74056190235eccbd
Alex Lobtsov <dessander@gmail.com>
2016-01-04 13:59:36 +0400

    add node class

---
+++ source/Apishka/Templater/Node.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Apishka templater node
+ *
+ * @uses Apishka_Templater_NodeAbstract
+ * @author Alexander "grevus" Lobtsov <alex@lobtsov.com>
+ */
+
+class Apishka_Templater_Node extends Apishka_Templater_NodeAbstract
+{
+}
